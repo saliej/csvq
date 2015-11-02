016557004069fa048abd9a6b4fd3143546b376cc
Salie Jakoet <saliej@gmail.com>
2015-11-02 16:42:34 +0000

    Added bulk insert support Added support for non-existent header rows

--- csvq.go
+++ csvq.go
@@ -24,13 +24,15 @@ func main() {
 	var query = flag.String("q", "SELECT sqlite_version()", "The query to run over the files.  Omit to retrieve the SQLite library version.")
 	var dbName = flag.String("db", ":memory:", "The name of the database to create.  Omit to create an in-memory database")
 	var dm = flag.String("dl", ",", "The field delimiter.  The default is a comma")
-	var keepExtension = false //flag.Bool("ke", false, "Whether or not the file extension should saved as part of the table name")
+	var keepExtension = flag.Bool("ke", false, "Whether or not the file extension should saved as part of the table name")
+	var hasHeader = flag.Bool("he", true, "Whether or not the input files have a header row.")
 
 	flag.Parse();
 
 	options := ProgramOptions{}
-	options.delimiter, _ = utf8.DecodeRuneInString(*dm);
-	options.keepExtension = keepExtension;
+	options.delimiter, _ = utf8.DecodeRuneInString(*dm)
+	options.keepExtension = *keepExtension
+	options.hasHeader = *hasHeader
 
 	db, err := sqlx.Open("sqlite3", *dbName)
 	checkErr(err)
@@ -44,7 +46,7 @@ func main() {
 
 	// This will store the set of files
 	// We store it as a set so that self joins don't result
-	// in the same tables being created twice
+	// in the same tables being created twic
 	fileSet := make(map[string]struct{})
 
 	var modifiedQuery = *query
@@ -61,13 +63,13 @@ func main() {
 		modifiedQuery = strings.Replace(modifiedQuery, match[0], "\"" + filePath + "\"", -1)
 		fileSet[filePath] = struct{}{}
 
-		// fmt.Printf("%s\n", modifiedQuery)
+		//fmt.Printf("%s\n", modifiedQuery)
 	}
 
 	// Add any extra files specified in the command line
 	for i := 0; i < flag.NArg(); i++ {
-
 		var filePath = strings.TrimSpace(flag.Arg(i))
+		fmt.Printf("%s\n", filePath)
 
 		// Check that this file exists
 		if _, err := os.Stat(filePath); err != nil {
@@ -76,6 +78,7 @@ func main() {
 		}
 
 		fileSet[filePath] = struct{}{}
+
 	}
 
 
@@ -117,6 +120,9 @@ func createTableFromFile(db *sqlx.DB, fileName string, options ProgramOptions) s
 
 	var createStatement = `CREATE TABLE IF NOT EXISTS "%s" (`
 	var tableName string
+	var insertStatement = ``
+	
+	const BATCH_SIZE int = 200
 
 	for i, line := range lines {
 
@@ -126,14 +132,21 @@ func createTableFromFile(db *sqlx.DB, fileName string, options ProgramOptions) s
 					createStatement += ", "
 				}
 
-				column := fmt.Sprintf(`"%s" TEXT NULL`, line[j])
+				var column string
+				if options.hasHeader {
+					column = fmt.Sprintf(`"%s" TEXT NULL`, line[j])
+				} else {
+					column = fmt.Sprintf(`"Column%d" TEXT NULL`, j + 1)
+
+				}
+
 				createStatement += column
+
 			}
 
 			createStatement += ");"
 
 			
-
 			if options.keepExtension {
 				tableName = fileName[0:len(fileName)]
 			} else {
@@ -143,33 +156,56 @@ func createTableFromFile(db *sqlx.DB, fileName string, options ProgramOptions) s
 
 			createStatement = fmt.Sprintf(createStatement, tableName)
 
-			// fmt.Println(createStatement)
+			execSql(db, createStatement)
 
-			stmt, err := db.Prepare(createStatement)
-			checkErr(err)
-
-			_, err = stmt.Exec()
-
-			checkErr(err)
-
-			continue
+			if options.hasHeader {
+				continue
+			}
 		}
 
-		var insertStatement = `INSERT INTO "%s" VALUES ("%s")`
-		insertStatement = fmt.Sprintf(insertStatement, tableName, strings.Join(line, `","`))
-		//  fmt.Println(insertStatement);
-		stmt, err := db.Prepare(insertStatement)
-		checkErr(err)
+		// Perform a bulk insert
+		var row int
+		if options.hasHeader {
+			row = i - 1
+		} else {
+			row = i
+		}
 
-		_, err = stmt.Exec()
+		if row == 0 || (row % BATCH_SIZE) == 0 {
+			// Insert the current batch
+			if len(insertStatement) > 0 {
 
-		checkErr(err)
+				execSql(db, insertStatement)
+				insertStatement = ""
 
-		//break
+			}
+
+			// Start a new batch
+			insertStatement = `INSERT INTO "%s" VALUES ("%s")`
+			insertStatement = fmt.Sprintf(insertStatement, tableName, strings.Join(line, `","`))
+		} else {
+			insertStatement += `, ("%s")` + "\n"
+			insertStatement = fmt.Sprintf(insertStatement, strings.Join(line, `","`))	
+		}
+	}
+
+	// Insert the final batch
+	if len(insertStatement) > 0 {
+		// fmt.Println(insertStatement)
+		execSql(db, insertStatement)
 	}
 
 	return tableName
 
+}
+
+func execSql(db *sqlx.DB, sql string) {
+	stmt, err := db.Prepare(sql)
+	checkErr(err)
+
+	_, err = stmt.Exec()
+
+	checkErr(err)
 }
 
 func checkErr(err error) {
